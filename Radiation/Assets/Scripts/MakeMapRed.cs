0272a467aadf5ea2485bab8387f5a7f5c7d25d4c
limejuice123 <s175883@ucs.ac.uk>
2018-02-19 11:50:23 +0000

    Added Radiation to Repository

---
+++ Radiation/Assets/Scripts/MakeMapRed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeMapRed : MonoBehaviour 
+{
+	public SpriteRenderer[] mapSprites;
+	public float[] timerPoints;
+	public float timer;
+
+	void Start()
+	{
+		for (int i = 0; i < timerPoints.Length; i++) 
+		{
+			timerPoints [i] = Random.Range (20f, 50f);
+		}
+	}
+
+	void LerpColour()
+	{
+		for (int y = 0; y < timerPoints.Length; y++) 
+		{
+			
+		}
+	}
+
+	void Update () 
+	{
+		timer = timer + Time.deltaTime;
+	}
+}
